4d4bc0fb2c9061bc596897be6ecc38e79f50d475
nishatlea <nishataurora1956@gmail.com>
2022-11-07 11:42:27 +0600

    validation check

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Blog_in_Ruby_on_rails
+References:
+#https://guides.rubyonrails.org/getting_started.html
+#https://www.youtube.com/watch?v=fmyvWz5TUWg
+
+config/routes.rb
+link_to helper
+Form builder
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
+  get "/articles_new", to:"articles#new"
   resources :articles
 end
